30f035c84260bf20bebdf35b3833149ab8ba4155
alex-polosky <alexpolosky@gmail.com>
2023-08-29 11:38:46 -0400

    loading

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
 import { Html5Qrcode } from "html5-qrcode";
 
-const HTML_ELEMENT_ID = 'reader';
-var camId = '';
-Html5Qrcode.getCameras().then(devices => {
-    if (devices && devices.length) {
-        camId = devices[0].id;
-        const html5QrCode = new Html5Qrcode(HTML_ELEMENT_ID);
-        html5QrCode.start(
-            camId,
-            {
-                fps: 10,
-                qrbox: {width: 250, height: 250}
-            },
-            (decodedText, decodedResult) => {
-                console.log(decodedText);
-            },
-            (errorMessage) => {
-                console.error(errorMessage);
-            }
-        ).catch((err) => {
-            console.error(err);
-        })
-    }
-}).catch(err => console.error(err));
+document.body.onload = () => {
+    const HTML_READER_ELEMENT_ID = 'reader';
+    const HTML_OUTPUT_ELEMENT_ID = 'output';
+
+    const output_elem: HTMLElement = document.getElementById(HTML_OUTPUT_ELEMENT_ID) as HTMLElement;
+
+    var camId = '';
+    Html5Qrcode.getCameras().then(devices => {
+        if (devices && devices.length) {
+            camId = devices[0].id;
+            const html5QrCode = new Html5Qrcode(HTML_READER_ELEMENT_ID);
+            html5QrCode.start(
+                camId,
+                {
+                    fps: 10,
+                    qrbox: {width: 250, height: 250}
+                },
+                (decodedText, decodedResult) => {
+                    // console.log(decodedText);
+                    (output_elem as any).value = decodedText;
+                },
+                (errorMessage) => {
+                    if (errorMessage.indexOf('No MultiFormat Readers were able to detect the code.') < 0) {
+                        console.error(errorMessage);
+                    }
+                }
+            ).catch((err) => {
+                console.error(err);
+            })
+        }
+    }).catch(err => console.error(err));
+}
+
